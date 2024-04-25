888a8b3a2edb34ff0b3deabebba12f208ceafce5
Curtis John <admin@curtisrayjohn.com>
2024-04-24 21:23:07 -0400

    refactor(deemix): create directories if they don't exist on startup by running user

--- apps/deemix/entrypoint.sh
+++ apps/deemix/entrypoint.sh
@@ -5,6 +5,15 @@ export DEEMIX_MUSIC_DIR="$DEEMIX__MUSIC_DIR"
 export DEEMIX_HOST="$DEEMIX__HOST"
 export DEEMIX_PORT="$DEEMIX__PORT"
 
+# make sure directories exist
+for DIRECTORY in $DEEMIX_DATA_DIR $DEEMIX_MUSIC_DIR
+do
+  if [ ! -d "$DIRECTORY" ]; then
+    echo "Creating $DIRECTORY"
+    mkdir -p $DIRECTORY
+  fi
+done
+
 #shellcheck disable=SC2086
 exec \
     /app/bin/deemix-server
